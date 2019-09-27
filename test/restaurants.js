b1d41444f440d8c9e015a27a1d7dd803493396ca
hi15 <hi15@inf.ufpr.br>
2019-09-27 11:03:23 -0300

    Added test to get best restaurants near address

--- test/restaurants.js
+++ test/restaurants.js
@@ -16,6 +16,22 @@ describe('Restaurants', () => {
                 should.exist(res.body);
                 res.should.have.status(200);
                 res.should.be.a('object');
+                res.body.should.have.property('categories');
+                done();
+            });
+    });
+
+    it('should get best restaurants for new york city', (done) => {
+        chai.request(server)
+            .get('/best_near?place=New York City')
+            .end((err, res) => {
+                should.exist(res.body);
+                res.should.have.status(200);
+                res.should.be.a('object');
+                res.body.should.have.property('location');
+                res.body.should.have.property('popularity');
+                res.body.should.have.property('link');
+                res.body.should.have.property('nearby_restaurants');
                 done();
             });
     });
